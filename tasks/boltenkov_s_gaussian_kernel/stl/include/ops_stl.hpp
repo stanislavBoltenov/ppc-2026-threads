1a6d43aa0975aa0fd75b9bd2a891a913d098a668
theProgrammerOfOurTime <azarova.v2104@gmail.com>
2026-05-12 22:07:16 +0300

    fix clang-tidy

--- tasks/boltenkov_s_gaussian_kernel/stl/include/ops_stl.hpp
+++ tasks/boltenkov_s_gaussian_kernel/stl/include/ops_stl.hpp
@@ -21,6 +21,10 @@ class BoltenkovSGaussianKernelSTL : public BaseTask {
   bool PreProcessingImpl() override;
   bool RunImpl() override;
   bool PostProcessingImpl() override;
+  void CopyRowsToPaddedBuffer(const std::vector<std::vector<int>> &data, std::vector<std::vector<int>> &tmp_data,
+                              std::size_t n, unsigned int num_threads);
+  void ApplyGaussianKernel(const std::vector<std::vector<int>> &tmp_data, std::vector<std::vector<int>> &res,
+                           std::size_t n, std::size_t m, unsigned int num_threads);
 };
 
 }  // namespace boltenkov_s_gaussian_kernel
--- tasks/boltenkov_s_gaussian_kernel/stl/src/ops_stl.cpp
+++ tasks/boltenkov_s_gaussian_kernel/stl/src/ops_stl.cpp
@@ -39,80 +39,73 @@ bool BoltenkovSGaussianKernelSTL::PreProcessingImpl() {
   return true;
 }
 
+void BoltenkovSGaussianKernelSTL::CopyRowsToPaddedBuffer(const std::vector<std::vector<int>> &data,
+                                                         std::vector<std::vector<int>> &tmp_data, std::size_t n,
+                                                         unsigned int num_threads) {
+  std::vector<std::thread> threads;
+  std::size_t rows_per_thread = n / num_threads;
+  std::size_t remainder = n % num_threads;
+  std::size_t start = 1;
+  for (unsigned int tid = 0; tid < num_threads; ++tid) {
+    std::size_t count = rows_per_thread + (tid < remainder ? 1 : 0);
+    std::size_t end = start + count;
+    threads.emplace_back([&data, &tmp_data, start, end] {
+      for (std::size_t i = start; i < end; ++i) {
+        std::copy(data[i - 1].begin(), data[i - 1].end(), tmp_data[i].begin() + 1);
+      }
+    });
+    start = end;
+  }
+  for (auto &th : threads) {
+    th.join();
+  }
+}
+
+void BoltenkovSGaussianKernelSTL::ApplyGaussianKernel(const std::vector<std::vector<int>> &tmp_data,
+                                                      std::vector<std::vector<int>> &res, std::size_t n, std::size_t m,
+                                                      unsigned int num_threads) {
+  auto kernel = kernel_;
+  int shift = shift_;
+  std::vector<std::thread> threads;
+  std::size_t rows_per_thread = n / num_threads;
+  std::size_t remainder = n % num_threads;
+  std::size_t start = 1;
+  for (unsigned int tid = 0; tid < num_threads; ++tid) {
+    std::size_t count = rows_per_thread + (tid < remainder ? 1 : 0);
+    std::size_t end = start + count;
+    threads.emplace_back([&tmp_data, &res, kernel, shift, start, end, m] {
+      for (std::size_t i = start; i < end; ++i) {
+        for (std::size_t j = 1; j <= m; ++j) {
+          int sum = (tmp_data[i - 1][j - 1] * kernel[0][0]) + (tmp_data[i - 1][j] * kernel[0][1]) +
+                    (tmp_data[i - 1][j + 1] * kernel[0][2]) + (tmp_data[i][j - 1] * kernel[1][0]) +
+                    (tmp_data[i][j] * kernel[1][1]) + (tmp_data[i][j + 1] * kernel[1][2]) +
+                    (tmp_data[i + 1][j - 1] * kernel[2][0]) + (tmp_data[i + 1][j] * kernel[2][1]) +
+                    (tmp_data[i + 1][j + 1] * kernel[2][2]);
+          res[i - 1][j - 1] = sum >> shift;
+        }
+      }
+    });
+    start = end;
+  }
+  for (auto &th : threads) {
+    th.join();
+  }
+}
+
 bool BoltenkovSGaussianKernelSTL::RunImpl() {
   std::size_t n = std::get<0>(GetInput());
   std::size_t m = std::get<1>(GetInput());
-
   std::vector<std::vector<int>> data = std::get<2>(GetInput());
   std::vector<std::vector<int>> tmp_data(n + 2, std::vector<int>(m + 2, 0));
   std::vector<std::vector<int>> &res = GetOutput();
 
-  unsigned int num_threads = static_cast<unsigned int>(ppc::util::GetNumThreads());
+  auto num_threads = static_cast<unsigned int>(ppc::util::GetNumThreads());
   if (num_threads == 0) {
     num_threads = 1;
   }
 
-  {
-    std::vector<std::thread> threads;
-    threads.reserve(num_threads);
-
-    std::size_t rows_per_thread = n / num_threads;
-    std::size_t remainder = n % num_threads;
-    std::size_t start = 1;
-
-    for (unsigned int t = 0; t < num_threads; ++t) {
-      std::size_t count = rows_per_thread + (t < remainder ? 1 : 0);
-      std::size_t end = start + count;
-
-      threads.emplace_back([&data, &tmp_data, start, end]() {
-        for (std::size_t i = start; i < end; ++i) {
-          std::copy(data[i - 1].begin(), data[i - 1].end(), tmp_data[i].begin() + 1);
-        }
-      });
-
-      start = end;
-    }
-
-    for (auto &t : threads) {
-      t.join();
-    }
-  }
-
-  auto kernel = kernel_;
-  int shift = shift_;
-
-  {
-    std::vector<std::thread> threads;
-    threads.reserve(num_threads);
-
-    std::size_t start = 1;
-    std::size_t rows_per_thread = n / num_threads;
-    std::size_t remainder = n % num_threads;
-
-    for (unsigned int t = 0; t < num_threads; ++t) {
-      std::size_t count = rows_per_thread + (t < remainder ? 1 : 0);
-      std::size_t end = start + count;
-
-      threads.emplace_back([&tmp_data, &res, kernel, shift, start, end, m]() {
-        for (std::size_t i = start; i < end; ++i) {
-          for (std::size_t j = 1; j <= m; ++j) {
-            int sum = (tmp_data[i - 1][j - 1] * kernel[0][0]) + (tmp_data[i - 1][j] * kernel[0][1]) +
-                      (tmp_data[i - 1][j + 1] * kernel[0][2]) + (tmp_data[i][j - 1] * kernel[1][0]) +
-                      (tmp_data[i][j] * kernel[1][1]) + (tmp_data[i][j + 1] * kernel[1][2]) +
-                      (tmp_data[i + 1][j - 1] * kernel[2][0]) + (tmp_data[i + 1][j] * kernel[2][1]) +
-                      (tmp_data[i + 1][j + 1] * kernel[2][2]);
-            res[i - 1][j - 1] = sum >> shift;
-          }
-        }
-      });
-
-      start = end;
-    }
-
-    for (auto &t : threads) {
-      t.join();
-    }
-  }
+  CopyRowsToPaddedBuffer(data, tmp_data, n, num_threads);
+  ApplyGaussianKernel(tmp_data, res, n, m, num_threads);
 
   return true;
 }
